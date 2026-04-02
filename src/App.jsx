cd8f30af226641b2e25d302d4f6b3fb66a1b59cc
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2026-04-02 15:52:47 -0400

    add score and click tracking login

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,44 @@ import { useState } from 'react';
 import useTrendingGifs from './services/giphyApi';
 import Footer from './components/Footer';
 import CardList from './components/CardList';
+import Scoreboard from './components/Scoreboard';
 
 function App() {
   const gifs = useTrendingGifs();
-  // const [cardScores, setCardScores] = useState(initialiazeScores());
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [clickedCards, setClickedCards] = useState(initialiazeCards());
 
-  // function initialiazeScores() {
-  //   const scores = {};
-  //   if (gifs.length > 0) {
-  //     gifs.map((gif) => {
-  //       scores[`${gif.id}`] = 0;
-  //     });
-  //   }
-  //   console.log(scores);
-  //   return scores;
-  // }
+  // key = card/GIF id, value = true, false (if it has been clicked)
+  function initialiazeCards() {
+    const cards = {};
+    const hasBeenClicked = false;
+    if (gifs.length > 0) {
+      gifs.map((gif) => {
+        cards[`${gif.id}`] = hasBeenClicked;
+      });
+    }
+    return cards;
+  }
 
   function handleCardClick(e) {
-    // if (e.target)
+    const card = e.currentTarget;
+    if (clickedCards[card.id]) {
+      setScore(0);
+      setClickedCards(initialiazeCards());
+    } else {
+      setClickedCards({ ...clickedCards, [card.id]: true });
+      setScore((prevScore) => {
+        const newScore = prevScore + 1;
+        newScore > bestScore && setBestScore(newScore);
+        return newScore;
+      });
+    }
   }
-  console.log(gifs);
 
   return (
     <>
+      <Scoreboard currentScore={score} bestScore={bestScore} />
       <CardList gifList={gifs} onClick={handleCardClick} />
       <Footer />
     </>
